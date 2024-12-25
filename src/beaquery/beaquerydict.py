d28ffeaf04c5580cbc6023159388f03c9de92b11
don caldwell <doncaldwell@mac.mynetworksettings.com>
2024-12-25 17:10:46 -0500

    .

--- src/beaquery/beaquerydict.py
+++ src/beaquery/beaquerydict.py
@@ -5,13 +5,15 @@ import sys
 import json
 import time
 import argparse
-import beaapi
 import pandas as pd
 
+import beaapi
 
-class BEAQuery():
+
+class BEAQueryDict():
     def __init__(self):
 
+        # BEA
         self.burl = 'https://apps.bea.gov/api/signup/'
         if 'BEA_API_KEY' in os.environ:
                 self.api_key = os.environ['BEA_API_KEY']
@@ -21,29 +23,41 @@ class BEAQuery():
                               file=sys.stderr)
             sys.exit()
 
-        # self.datasetdict = {}
+        self.delay = 2
 
+        # pandas
         pd.set_option('display.max_colwidth', None)
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
 
 
     def dsparamvals(self, dataset_name, parameter_name):
+        """ dsparamvals(dataset_name, parameter_name)
+        dataset_name - name of the dataset
+        parameter_name - name of the parameter
+        return pandas dataframe for the values of the dataset parameter
+        """
         print('Values for dataset %s parameter %s' % (dataset_name,
-                                                      parameter_name))
+                          parameter_name), file=sys.stderr)
         try:
-            pvals = beaapi.get_parameter_values(self.api_key,
-                                                     dataset_name,
+            pvalframe = beaapi.get_parameter_values(self.api_key,
+                            dataset_name,
                                                      parameter_name)
         except Exception as e:
             print('dsparamvals get_parameter_values %s' % e)
             sys.exit()
 
-        return pvals
+        time.sleep(self.delay)
+        return pvalframe
 
 
     def dsparams(self, dataset_name):
-        print('Parameters for %s dataset' % dataset_name)
+        """ dsparams(dataset_name)
+        dataset_name = name of the dataset
+        return pandas frame for the parameters of a dataset
+        """
+        print('Parameters for %s dataset' % dataset_name,
+        file=sys.stderr)
         try:
             paramframe = beaapi.get_parameter_list(self.api_key,
                                                        dataset_name)
@@ -51,16 +65,89 @@ class BEAQuery():
             print('dsparams get_parameter_list %s' % e)
             sys.exit()
 
+        time.sleep(self.delay)
         return paramframe
 
     def datasets(self):
+        """ datasets()
+        return pandas frame for BEA datasets
+        """
         try:
             datasetsframe = beaapi.get_data_set_list(self.api_key)
         except Exception as e:
             print('hierarchy get_data_set_list %s' % e)
             sys.exit()
 
+        time.sleep(self.delay)
         return datasetsframe
+
+    def jsondatasetshtml(self, dict):
+        """ jsondatasetshtml(dict)
+        render BEA dataset names parameters and values to html
+        dict = dictionary containing model data for datasets
+        return array containing an html rendering
+        """
+        ka = [k for k in dict['dataset'].keys()]
+        htmla = []
+        htmla.append('<table border=1 >')
+        hd = '</th><th scope="col">'.join(['DatasetName', 'DatasetDescription'])
+        htmla.append('<tr><th scope="col">%s</th></tr>' % (hd) )
+        cap = '<caption>%s</caption>' % ('BEA Datasets')
+        htmla.append(cap)
+        for k in ka:
+            ra = [k, dict['dataset'][k]]
+            rw = '</td><td scope="row">'.join(ra)
+            htmla.append('<tr><td scope="row">%s</td></tr>' % (rw) )
+        htmla.append('</table>')
+        for k in ka:
+            ht = self.jsonparametershtml(k, dict['dataset'][k])
+            htmla.extend(ht)
+            ht = self.jsonparametervalueshtml(k, dict['dataset'][k])
+            htmla.extend(ht)
+
+    def jsonparametershtml(self, ds, dict):
+        """ jsonparametershtml(ds, dict)
+        ds - dataset name
+        dict - dict containing data model for a dataset
+        return an html table rendering for the parameters of a dataset
+        """
+        ka = [k for k in dict['Parameters'][0].keys()]
+        htmla = []
+        htmla.append('<table border=1 >')
+        hd = '</th><th scope="col">'.join(ka)
+        htmla.append('<tr><th scope="col">%s</th></tr>' % (hd) )
+        cap = '<caption>%s</caption>' % ('BEA Dataset %s Parameters' % ds)
+        htmla.append(cap)
+        for d in dict['Parameters']:
+            ra = [d[k] for k in d.keys()]
+            rw = '</td><td scope="row">'.join(ra)
+            htmla.append('<tr><td scope="row">%s</td></tr>' % (rw) )
+        htmla.append('</table>')
+        return htmla
+
+    def jsonparametervalueshtml(self, ds, param, dict):
+        """ jsonparametervalueshtml(ds, param, dict)
+        ds - dataset name
+        param - parameter name
+        dict - dictionary containing schema for values of a parameter of a dataset
+        return an html table rendering for the values of the parameter
+        """
+        ka = [k for k in dict['ParameterValuess'][0].keys()]
+        htmla = []
+        htmla.append('<table border=1 >')
+        hd = '</th><th scope="col">'.join(ka)
+        htmla.append('<tr><th scope="col">%s</th></tr>' % (hd) )
+        cap = '<caption>%s</caption>' % ('BEA Datasets')
+        htmla.append(cap)
+        cap = '<caption>%s</caption>' % ('BEA Dataset %s Parameter %v Values' % (ds, param))
+        htmla.append(cap)
+        for d in dict['ParameterValuess']:
+            ra = [d[k] for k in d.keys()]
+            rw = '</td><td scope="row">'.join(ra)
+            htmla.append('<tr><td scope="row">%s</td></tr>' % (rw) )
+        htmla.append('</table>')
+        return htmla
+        pass
 
     def datasetdictparamval(self, i, pvdict):
         pvd = {}
@@ -69,36 +156,34 @@ class BEAQuery():
         return pvd
 
     def datasetdictparamvals(self, dsdict, dn):
-        dsdict['datasets'][dn]['ParameterValues'] = []
-        for pd in dsdict['datasets'][dn]['Parameters']:
-            pn =  pd['ParameterName']
-            pvals = self.dsparamvals(dn, pn)
-            pvalsdict = pvals.to_dict()
+        dsdict['datasets'][dn]['ParameterValues'] = {}
+        for i in range(1, len(dsdict['datasets'][dn]['Parameters'])):
+            pn = dsdict['datasets'][dn]['Parameters'][1][0]
+            pvalframe = self.dsparamvals(dn, pn)
+            pvalsdict = pvalframe.to_dict()
             ks = [k for k in pvalsdict.keys()]
-            for k in ks:
-                ix = [i for i in pvalsdict[k].keys()]
-            for k in ks:
-                for i in ix:
-                   pvdict = self.datasetdictparamval(i, pvalsdict)
-                   dsdict['datasets'][dn]['ParameterValues'].append(pvdict)
+            ix = [i for i in pvalsdict[ks[0]].keys()]
+            rows = []
+            rows.append(ks)
+            for i in ix:
+                rw = [pvalsdict[k][i] for k in ks]
+                rows.append(rw)
+            dsdict['datasets'][dn]['ParameterValues'][pn]=rows
 
-    def datasetdictparam(self, i, pdict):
-        pd = {}
-        for k in pdict.keys():
-                pd[k] = pdict[k][i]
-        return pd
+        return
 
     def datasetdictparams(self, dsdict, dn):
-        params = self.dsparams(dn)
-        paramsdict = params.to_dict()
-        dsdict['datasets'][dn]['Parameters'] = []
+        paramsframe = self.dsparams(dn)
+        paramsdict = paramsframe.to_dict()
         ks = [k for k in paramsdict.keys()]
-        for k in ks:
-            ix = [i for i in paramsdict[k].keys()]
-        for k in ks:
-            for i in ix:
-                pdict = self.datasetdictparam(i, paramsdict)
-                dsdict['datasets'][dn]['Parameters'].append(pdict)
+        ix = [i for i in paramsdict[ks[0]].keys()]
+        rows = []
+        rows.append(ks)
+        for i in ix:
+            rw = [paramsdict[k][i] for k in ks]
+            rows.append(rw)
+        dsdict['datasets'][dn]['Parameters'] = rows
+        return
 
     def initdatasetdict(self, dsf):
         dsr = dsf.to_dict()
@@ -145,11 +230,16 @@ def main():
 
     argp.add_argument('--json', action='store_true', default=False,
         help='display json')
+    argp.add_argument('--html', action='store_true', default=False,
+        help='display html')
     args=argp.parse_args()
 
-    BQ = BEAQuery()
+    BQ = BEAQueryDict()
     if args.hierarchy:
         dsdict = BQ.dicthierarchy()
+        if args.html:
+            htmla = BQ.jsondatasetshtml(dsdict)
+            print(''.join(htmla))
         if args.json:
             dsjson = json.JSONEncoder().encode(dsdict)
             print(dsjson)
@@ -174,12 +264,14 @@ def main():
         else:
             print(ps)
     elif args.paramvals:
-        if args.dataset == None and args.param == None:
+        if args.dataset == None or args.param == None:
             print('a dataset and parameter must be provided')
             argp.print_help()
             sys.exit()
         pvs = BQ.dsparamvals(args.dataset, args.param)
         print(pvs)
+
+    argp.print_help()
 
 
 
