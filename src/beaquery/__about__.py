dca03b31d8df675da5eb7e68ac9226f745c05b05
don caldwell <doncaldwell@dons-MacBook-Air.local>
2025-01-18 17:40:39 -0500

    .

--- src/beaquery/__about__.py
+++ src/beaquery/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
+# SPDX-FileCopyrightText: 2024-present Don Caldwell <dfwcnj@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.3"
+__version__ = "0.0.5"
